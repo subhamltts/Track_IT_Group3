91c32d144e3a7858f492ab158467a07d9fd9b65f
rajatsingh07 <rajat.singh1@ltts.com>
2021-08-12 19:25:05 +0530

    Updated

--- TrackIt/TrackIt_BL/ActivityBL.cs
+++ TrackIt/TrackIt_BL/ActivityBL.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackIt_DAL;
+using TrackIt_DTO;
 
 namespace TrackIt_BL
 {
     public class ActivityBL
     {
+        ActivityDAL objActivity;
+        public ActivityBL()
+        {
+            objActivity= new ActivityDAL();
+        }
+        public int AddNewActivity(ActivityDTO newActivityDetails)
+        {
+            try
+            {
+                int returnvalue = objActivity.AddNewActivity(newActivityDetails);
+                return returnvalue;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public int AddNewActivityTracker(ActivityTrackerDTO newActTrackerDetails)
+        {
+            try
+            {
+                int returnvalue = objActivity.AddNewActivityTracker(newActTrackerDetails);
+                return returnvalue;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
--- TrackIt/TrackIt_DAL/ActivityDAL.cs
+++ TrackIt/TrackIt_DAL/ActivityDAL.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackIt_DTO;
 
 namespace TrackIt_DAL
 {
     public class ActivityDAL
     {
+        SqlConnection sqlConObj;
+        SqlCommand sqlCmdObj;
+        public ActivityDAL()
+        {
+            sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["TrackItDTConStr"].ToString());
+        }
+        public int AddNewActivity(ActivityDTO ipActivity)
+        {
+            sqlCmdObj = new SqlCommand("dbo.uspInsertActivity", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@Activity_Id", ipActivity.Activity_Id);
+            sqlCmdObj.Parameters.AddWithValue("@CourseBatchId", ipActivity.CourseBatchId);
+            sqlCmdObj.Parameters.AddWithValue("@Activity_Name", ipActivity.Activity_Name);
+
+            sqlCmdObj.Parameters.AddWithValue("@Activity_SDT", ipActivity.Activity_SDT);
+            sqlCmdObj.Parameters.AddWithValue("@Activity_EDT", ipActivity.Activity_EDT);
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
+        public int AddNewActivityTracker(ActivityTrackerDTO ipActTracker)
+        {
+            sqlCmdObj = new SqlCommand("dbo.uspInsertActivityTracker", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@Activity_Id", ipActTracker.Activity_Id);
+            sqlCmdObj.Parameters.AddWithValue("@Activity_Name", ipActTracker.P_PSNo);
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
     }
 }
