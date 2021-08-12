39cc3b6ef48f0562e3be3888e19956bd684660c3
rajatsingh07 <rajat.singh1@ltts.com>
2021-08-12 17:39:24 +0530

    Updated

--- TrackIt/TrackIt_DTO/CourseDTO.cs
+++ TrackIt/TrackIt_DTO/CourseDTO.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace TrackIt_DTO
 {
-    class CourseDTO
+    public class CourseDTO
     {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
     }
 }
