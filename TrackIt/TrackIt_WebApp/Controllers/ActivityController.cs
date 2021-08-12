86d7e4c74825178012611677bf65b4c52bd8cca3
rajatsingh07 <rajat.singh1@ltts.com>
2021-08-12 20:02:51 +0530

    Updated

---
+++ TrackIt/TrackIt_WebApp/Controllers/ActivityController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TrackIt_BL;
+using TrackIt_DTO;
+
+namespace TrackIt_WebApp.Controllers
+{
+    public class ActivityController : ApiController
+    {
+        [HttpPost]
+        [ActionName("AddActivities")]
+        public HttpResponseMessage AddFaculties(ActivityDTO ipActObj)
+        {
+            ActivityBL objActivity;
+            try
+            {
+
+                if (ipActObj != null && ipActObj.Activity_Id != null && ipActObj.CourseBatchId != null && ipActObj.Activity_Name != null && ipActObj.Activity_SDT != null && ipActObj.Activity_EDT != null)
+                {
+                    objActivity = new ActivityBL();
+                    int retVal = objActivity.AddNewActivity(ipActObj);
+                    if (retVal == 1)
+                    {
+                        var response = new HttpResponseMessage(HttpStatusCode.OK);
+                        response.Content = new StringContent("Data Added Successfully");
+                        return response;
+                    }
+                    else
+                    {
+                        var response = new HttpResponseMessage(HttpStatusCode.OK);
+                        response.Content = new StringContent("Data Not Added");
+                        return response;
+                    }
+                }
+                else
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.NoContent);
+                    response.Content = new StringContent("Some Details are missing");
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                response.Content = new StringContent("Opppsssiiieee, SomeThing went wrong");
+                return response;
+            }
+        }
+    }
+}
