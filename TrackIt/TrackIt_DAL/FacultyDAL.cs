037bd0d5e518b9e5ba56dac903be8bab97e1da2e
subhamltts <subham.panda@ltts.com>
2021-08-13 09:44:33 +0530

    updated

--- TrackIt/TrackIt_DAL/FacultyDAL.cs
+++ TrackIt/TrackIt_DAL/FacultyDAL.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackIt_DTO;
 
 namespace TrackIt_DAL
 {
     public class FacultyDAL
     {
+        SqlConnection sqlConObj = null;
+        SqlCommand sqlCmdObj = null;
+
+        public FacultyDAL()
+        {
+            sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["TrackItDTConStr"].ToString());
+        }
+        public int GetAllFacultyDetails(int F_PSNo, string F_EmailId, string F_Name)
+        {
+            
+            sqlCmdObj = new SqlCommand("dbo.uspInsertFacultyDetails", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@FacultyId", F_PSNo);
+            sqlCmdObj.Parameters.AddWithValue("@FacultyEmail", F_EmailId);
+            sqlCmdObj.Parameters.AddWithValue("@FacultyName", F_Name);
+            
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
+
+        public int AddNewFacultyDetails(int F_PSNo, string F_EmailId, string F_Name)
+        {
+            sqlCmdObj = new SqlCommand("dbo.uspInsertFaculty", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@FacultyId", F_PSNo);
+            sqlCmdObj.Parameters.AddWithValue("@FacultyEmail", F_EmailId);
+            sqlCmdObj.Parameters.AddWithValue("@FacultyName", F_Name);
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
+
     }
 }
--- TrackIt/TrackIt_DAL/ParticipantDAL.cs
+++ TrackIt/TrackIt_DAL/ParticipantDAL.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,65 @@ namespace TrackIt_DAL
 {
     public class ParticipantDAL
     {
+        SqlConnection sqlConObj = null;
+        SqlCommand sqlCmdObj = null;
+
+        public ParticipantDAL()
+        {
+            sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["TrackItDTConStr"].ToString());
+        }
+        public int GetAllParticipantDetails(int P_PSNo, string P_EmailId, string P_Name)
+        {
+
+            sqlCmdObj = new SqlCommand("dbo.uspInsertParticipantDetails", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantId", P_PSNo);
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantEmail", P_EmailId);
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantName", P_Name);
+
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
+
+        public int AddNewParticipantDetails(int P_PSNo, string P_EmailId, string P_Name)
+        {
+            sqlCmdObj = new SqlCommand("dbo.uspInsertParticipant", sqlConObj);
+            sqlCmdObj.CommandType = CommandType.StoredProcedure;
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantId", P_PSNo);
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantEmail", P_EmailId);
+            sqlCmdObj.Parameters.AddWithValue("@ParticipantName", P_Name);
+            try
+            {
+                sqlConObj.Open();
+                SqlParameter returnManager = sqlCmdObj.Parameters.Add("RetVal", SqlDbType.Int);
+                returnManager.Direction = ParameterDirection.ReturnValue;
+                sqlCmdObj.ExecuteNonQuery();
+                int returnValue = (int)returnManager.Value;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+        }
     }
 }
